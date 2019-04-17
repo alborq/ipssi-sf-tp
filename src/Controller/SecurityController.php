f1077ec78b03031a6e6e1ddfb5c1f16d0c1fe62b
Lorddistrict <etienne.crespi@laposte.net>
2019-04-17 20:17:22 +0200

    Add register files - working progress

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,26 +2,52 @@
 
 namespace App\Controller;
 
+use App\Form\RegistrationType;
+use App\Form\UserRegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
 
-    public function login(AuthenticationUtils $authenticationUtils) : Response
-    {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+  public function login(AuthenticationUtils $authenticationUtils): Response
+  {
+    // get the login error if there is one
+    $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+    // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/loginForm.html.twig', [
-            'title' => 'Bet Rocket | Login',
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ]);
+    return $this->render('security/loginForm.html.twig', [
+      'title' => 'Bet Rocket | Login',
+      'last_username' => $lastUsername,
+      'error' => $error,
+    ]);
+  }
+
+  public function register(Request $request): Response
+  {
+    $form = $this->createForm(UserRegistrationType::class);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $user = $form->getData();
+
+      $entityManager = $this->getDoctrine()->getManager();
+
+      $entityManager->persist($user);
+
+      $entityManager->flush();
+
+      return $this->redirectToRoute('login');
     }
 
+    return $this->render('security/registerForm.html.twig', [
+      'UserRegistrationForm' => $form->createView()
+    ]);
+  }
 }

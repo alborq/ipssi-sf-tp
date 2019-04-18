3a6d9f41342d166826aee94190ba6578139e423f
WingsHell <kevin.alvespinto@gmail.com>
2019-04-18 14:44:07 +0200

    Add SignUp UserForm and modify article.html

---
+++ src/Controller/SignupController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserInscriptionType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SignupController extends AbstractController
+{
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route(path="/signUp")
+     */
+    public function signUp(Request $request): Response
+    {
+        $user = $this->createForm(UserInscriptionType::class)->handleRequest($request);
+        if($user->isSubmitted() && $user>isValid()){
+            $this->getDoctrine()->getManager()
+                ->persist($user->getData())
+                ->flush();
+        }
+        return $this->render('SignUp/addUser.html.twig', [
+            'userInscriptionForm' => $user->createView(),
+        ]);
+    }
+}

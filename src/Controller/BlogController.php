3f2cade9e57e67d5fde633f5bea9331aef1d305c
Fayçal BORDJAH <fbordjah@linagora.com>
2019-04-18 19:42:46 +0200

    Redirect to the blog page at the root uri

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,15 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- *@Route(path="/blog",name="blog")
- */
 class BlogController extends AbstractController
 {
-    /**
-     *@Route(path="/articles")
-     */
-    public function getAllArictles() :Response
+    public function index() :Response
     {
         return $this->render('base.html.twig');
     }

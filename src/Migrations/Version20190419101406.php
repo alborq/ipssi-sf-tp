1e0c1ac2289e00480851a86faef386156970c37e
AlexandreBalle <alexballe35@gmail.com>
2019-04-19 14:13:19 +0200

    update

--- src/Migrations/Version20190419101406.php
+++ src/Migrations/Version20190419101406.php
@@ -20,8 +20,10 @@ final class Version20190419101406 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
 
         $this->addSql('ALTER TABLE advert ADD comment_enabled TINYINT(1) NOT NULL');
     }
@@ -29,8 +31,10 @@ final class Version20190419101406 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
 
         $this->addSql('ALTER TABLE advert DROP comment_enabled');
     }

02531b209afbc0209f5977c41103461777170297
AlexandreBalle <alexballe35@gmail.com>
2019-04-19 15:59:37 +0200

    update

--- src/Controller/Security/AdminController.php
+++ src/Controller/Security/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Security;
 
+use App\Entity\Advert;
 use App\Entity\Game;
+use App\Repository\AdvertRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,6 +27,12 @@ class AdminController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $games = $em->getRepository(Game::class)->findAll();
 
+        $em = $this->getDoctrine()->getManager();
+
+        /** @var AdvertRepository $repo */
+        $repo = $em->getRepository(Advert::class);
+
+        $allAppointmentsQuery = $repo->findAdvertByDate();
         return $this->render("Security/dashboard.html.twig", array(
             'games' => $games,
         ));

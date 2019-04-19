ad515434f3b428aa30ca4bf05df0ebfd38f20e06
Sebastien <sebguy.hierso@gmail.com>
2019-04-19 14:47:55 +0200

    19/04

--- src/Migrations/Version20190419092015.php
+++ src/Migrations/Version20190419092015.php
@@ -20,16 +20,20 @@ final class Version20190419092015 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() 
+        !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article CHANGE content content VARCHAR(255) NOT NULL, CHANGE comment comment TINYINT(1) NOT NULL, CHANGE date_article date_article DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE article CHANGE content content VARCHAR(255) NOT NULL, 
+        CHANGE comment comment TINYINT(1) NOT NULL, CHANGE date_article date_article DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 
+        'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article CHANGE content content VARCHAR(10000) NOT NULL COLLATE latin1_swedish_ci, CHANGE comment comment TINYINT(1) DEFAULT NULL, CHANGE date_article date_article DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE article CHANGE content content VARCHAR(10000) NOT NULL COLLATE latin1_swedish_ci, 
+        CHANGE comment comment TINYINT(1) DEFAULT NULL, CHANGE date_article date_article DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
     }
 }

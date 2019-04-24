04fb5e81366acf23e13edd0e6debf1261c7a01fb
Lorddistrict <etienne.crespi@laposte.net>
2019-04-25 01:00:23 +0200

    Add censure wp

--- src/Migrations/Version20190424222621.php
+++ src/Migrations/Version20190424222621.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190423202250 extends AbstractMigration
+final class Version20190424222621 extends AbstractMigration
 {
     public function getDescription() : string
     {
